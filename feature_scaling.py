3b15e4496480fec1d64048c00626659007d97ebb
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-06-12 06:32:40 +0200

    feature: added min-max scaling to feature scaling example

--- feature_scaling.py
+++ feature_scaling.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import MinMaxScaler
+
+
 df = pd.read_csv('/Users/kinga/Documents/DataScience/feature_scaling/SampleFile.csv')
 
 
@@ -9,3 +12,11 @@ df = pd.read_csv('/Users/kinga/Documents/DataScience/feature_scaling/SampleFile.
 max_vals = np.max(np.abs(df))
 
 scaled_df = ((df - max_vals) / max_vals)
+
+
+# min-max scaling
+
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled_data,
+                         columns=df.columns)
