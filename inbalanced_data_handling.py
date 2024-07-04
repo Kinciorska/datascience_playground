abc8c9cdcbf6b7f66d3f9576f2a81b2f7795ad99
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-07-04 16:22:28 +0200

    feature: added BalancedBaggingClassifier to inbalanced data handling example

--- inbalanced_data_handling.py
+++ inbalanced_data_handling.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 from imblearn.under_sampling import NearMiss
 from imblearn.over_sampling import SMOTE
+from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 
 
@@ -64,3 +67,33 @@ lr2.fit(X_train_nm, y_train_nm.ravel())
 predictions_after_near_miss = lr2.predict(X_test)
 
 print(classification_report(y_test, predictions_after_near_miss))
+
+
+# The BalancedBaggingClassifier is used with imbalanced datasets, as it balances training
+# by incorporating a parameter "sampling strategy" which determines the type of resampling
+
+X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+                           n_informative=3, n_redundant=1, flip_y=0,
+                           n_features=20, n_clusters_per_class=1,
+                           n_samples=1000, random_state=42)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+base_classifier = RandomForestClassifier(random_state=42)
+
+balanced_bagging_classifier = BalancedBaggingClassifier(base_classifier,
+                                                        sampling_strategy='auto',
+                                                        replacement=False,
+                                                        random_state=42)
+
+balanced_bagging_classifier.fit(X_train, y_train)
+
+
+y_predicted = balanced_bagging_classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_predicted)
+
+print("Accuracy:", accuracy)
+print("Classification Report:\n", classification_report(y_test, y_predicted))
