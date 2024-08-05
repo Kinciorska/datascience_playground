df3f2b42321b0ea7722338327994306d275e180b
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-08-05 15:26:36 +0200

    feature: expanded the  k-nearest neighbour example to compute the best k value

--- classification/k-nearest_neighbour.py
+++ classification/k-nearest_neighbour.py
@@ -1,3 +1,8 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
@@ -19,3 +24,31 @@ model.fit(X_train, y_train)
 
 print(model.predict(X_test))
 print(model.score(X_test, y_test))
+
+
+# for best k value we need to test an appropriate range of k values
+
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# loop over k values
+for i, k in enumerate(neighbors):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+
+    # compute training and test data accuracy
+    train_accuracy[i] = knn.score(X_train, y_train)
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# generate plot
+plt.plot(neighbors, test_accuracy, label='Testing dataset Accuracy')
+plt.plot(neighbors, train_accuracy, label='Training dataset Accuracy')
+
+plt.legend()
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
